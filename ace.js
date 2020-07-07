928296dc197c3de0b4f08fc5ccae814c446c9ca4
Anita-Jamgbadi <jamgbanita@gmail.com>
2020-07-07 02:18:39 +0100

    new

--- ace.js
+++ ace.js
@@ -1,12 +1,32 @@
-var back = document.getElementById('back');
+/*var back = document.getElementById('back');
 back.addEventListener('click', goBack);
 function goBack() {
   window.history.back()
 }
-
+*/
 window.addEventListener('load', load);
 const loader = document.querySelector('.loader-bg');
 
 function load(){
   loader.className += " hidden"
 }
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  autoplay: true,
+  autoplayTimeout:1000,
+  responsive:{
+      0:{
+          items:4
+      },
+      600:{
+          items:6
+      },
+      1000:{
+          items:9
+      }
+  }
+})
+
